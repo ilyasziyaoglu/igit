18fb5a17bc427d04b216d60fef250ca5a4e970bd
Ilyas Ziyaoglu <ilyasziyaoglu@gmail.com>
2020-07-25 16:51:15 +0300

    added permission settings

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+echo "Configuring igit.."
 echo "pardir=./"> igit.config
 echo "igitdir=~/igit" >> igit.config
 echo "alias \"igit=~/igit/igit.sh\"" >> ~/.bash_profile
+echo "Setting permitions.."
+chmod 755 igit.config
+chmod 755 igit.sh
+chmod 755 ubrs.sh
 mv `pwd` ~/
 echo SUCCESS!
